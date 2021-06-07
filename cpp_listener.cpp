549f316bf27e86fd3de6be1711b487bd4e56de08
HONG-C <jihun3900@naver.com>
2021-06-07 13:37:31 +0900

    cpp_listener의 메시지 수신 성공,기존 pyduino에서 사용되는 아두이노 파일 사용 가능

--- cpp_listener.cpp
+++ cpp_listener.cpp
@@ -1,14 +1,13 @@
 #include "ros/ros.h"                          // ROS Default Header File
+#include "std_msgs/Float64.h"
 #include "cppduino/MsgTutorial.h"  // MsgTutorial Message File Header. The header file is automatically created when building the package.
 
 // Message callback function. This is a function is called when a topic
 // message named 'ros_tutorial_msg' is received. As an input message,
 // the 'MsgTutorial' message of the 'ros_tutorials_topic' package is received.
-void msgCallback(const cppduino::MsgTutorial::ConstPtr& msg)
+void msgCallback(const std_msgs::Float64 msg)
 {
-  ROS_INFO("recieve msg = %d", msg->stamp.sec);   // Prints the 'stamp.sec' message
-  ROS_INFO("recieve msg = %d", msg->stamp.nsec);  // Prints the 'stamp.nsec' message
-  ROS_INFO("recieve msg = %d", msg->data);        // Prints the 'data' message
+  ROS_INFO("recieve msg = %f", msg.data);        // Prints the 'data' message
 }
 
 int main(int argc, char **argv)                         // Node Main Function
@@ -20,7 +19,7 @@ int main(int argc, char **argv)                         // Node Main Function
   // Declares subscriber. Create subscriber 'ros_tutorial_sub' using the 'MsgTutorial'
   // message file from the 'ros_tutorials_topic' package. The topic name is
   // 'ros_tutorial_msg' and the size of the publisher queue is set to 100.
-  ros::Subscriber cppduino_sub = nh.subscribe("ros_tutorial_msg", 100, msgCallback);
+  ros::Subscriber cppduino_sub = nh.subscribe("my_topic", 100, msgCallback);
 
   // A function for calling a callback function, waiting for a message to be
   // received, and executing a callback function when it is received.
