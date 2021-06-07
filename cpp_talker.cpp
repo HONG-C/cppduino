33cbab5c1a039c2cd88dbbfcf17088a3ea199f1b
HONG-C <jihun3900@naver.com>
2021-06-07 13:11:20 +0900

    cpp_talker의 아두이노와 메시지 통신 성공

--- cpp_talker.cpp
+++ cpp_talker.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"                            // ROS Default Header File
+#include "std_msgs/Float64.h"
 #include "cppduino/MsgTutorial.h"    // MsgTutorial Message File Header. The header file is automatically created when building the package.
 
 int main(int argc, char **argv)                 // Node Main Function
@@ -9,28 +10,26 @@ int main(int argc, char **argv)                 // Node Main Function
   // Declare publisher, create publisher 'ros_tutorial_pub' using the 'MsgTutorial'
   // message file from the 'ros_tutorials_topic' package. The topic name is
   // 'ros_tutorial_msg' and the size of the publisher queue is set to 100.
-  ros::Publisher cppduino_pub = nh.advertise<cppduino::MsgTutorial>("ros_tutorial_msg", 100);
+  ros::Publisher cppduino_pub = nh.advertise<std_msgs::Float64>("your_topic", 100);
 
   // Set the loop period. '10' refers to 10 Hz and the main loop repeats at 0.1 second intervals
   ros::Rate loop_rate(10);
 
-  cppduino::MsgTutorial msg;     // Declares message 'msg' in 'MsgTutorial' message file format
-  int count = 0;                            // Variable to be used in message
+  std_msgs::Float64 msg;
+  float count = 11.23;                            // Variable to be used in message
 
   while (ros::ok())
   {
-    msg.stamp = ros::Time::now();           // Save current time in the stamp of 'msg'
+
     msg.data  = count;                      // Save the the 'count' value in the data of 'msg'
 
-    ROS_INFO("send msg = %d", msg.stamp.sec);   // Prints the 'stamp.sec' message
-    ROS_INFO("send msg = %d", msg.stamp.nsec);  // Prints the 'stamp.nsec' message
-    ROS_INFO("send msg = %d", msg.data);        // Prints the 'data' message
+
+    ROS_INFO("send msg = %f", msg.data);        // Prints the 'data' message
 
     cppduino_pub.publish(msg);          // Publishes 'msg' message
 
     loop_rate.sleep();                      // Goes to sleep according to the loop rate defined above.
 
-    ++count;                                // Increase count variable by one
   }
 
   return 0;
